662a20452b85deccf9700b8e3c7db7ade74aaa11
24LP0282 24LP0282 <24LP0282@administrators-iMac.local>
2025-09-15 16:16:50 +0800

    user question frontend

---
+++ frontend/src/app/question/page.tsx
@@ -0,0 +1,12 @@
+import FourOptions from "./_components/FourOptions";
+import SentenceMatching from "./_components/SentenceMatching";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-gray-50 p-6 bg-zinc-100 ">
+      <FourOptions />
+      <SentenceMatching/>
+    </main>
+  );
+}
+
