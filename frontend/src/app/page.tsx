63ab2b96849f70827d5a4b20836651d8d78334e2
odgarag <odgarag.odko@gmail.com>
2025-09-19 13:08:21 +0800

    .

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,19 @@
-"use client";
+'use client'
 
-import { Button } from "@/components/ui/button";
-import { useGetUsersQuery } from "../../graphql/generated";
-import CreateUserForm from "./login/_components/user-form";
+import { Button } from '@/components/ui/button'
+import { useGetUsersQuery } from '../../graphql/generated'
+import CreateUserForm from './login/_components/user-form'
 
 const Home = () => {
-  const { data } = useGetUsersQuery();
+  const { data } = useGetUsersQuery()
 
-  console.log(data?.getUsers);
+  console.log(data?.getUsers)
 
   return (
     <div className="">
       <Button variant="outline">Addd</Button>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
